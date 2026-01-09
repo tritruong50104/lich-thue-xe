849685647d6625c6329ecaeb9fd0ca33621483c8
tritruong50104 <5wcwsqrn2q@privaterelay.appleid.com>
2026-01-09 19:53:13 +0700

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-const CACHE_NAME='lich-thue-v1';
-const FILES=['./','./index.html','./manifest.json'];
-self.addEventListener('install',e=>{
- e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(FILES)));
-});
-self.addEventListener('fetch',e=>{
- e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
-});
