e63de512004b5be285744dbfd6a07da36cd605f9
tritruong50104 <5wcwsqrn2q@privaterelay.appleid.com>
2026-01-09 16:45:53 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,8 @@
+const CACHE_NAME='lich-thue-v1';
+const FILES=['./','./index.html','./manifest.json'];
+self.addEventListener('install',e=>{
+ e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(FILES)));
+});
+self.addEventListener('fetch',e=>{
+ e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+});
